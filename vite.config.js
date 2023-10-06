7d415b40ec1d89f9abf8336f0fe0099085a98ce0
yo-404 <yogeshy2801@gmail.com>
2023-10-06 06:37:56 +0530

    deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yo-404.github.io",
+  base: "/yo-404.github.io/",
   plugins: [react()],
 })
